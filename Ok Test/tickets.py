024c1d2130cf72f71b3dd076cd9fdcd13d30e510
Christofer Tibbelin <christofer@tibbelin.net>
2025-08-12 19:48:48 +0200

    fixed folder structure

---
+++ Ok Test/tickets.py
@@ -0,0 +1,22 @@
+
+# Sample customer support tickets
+
+tickets = [
+    """
+    I ordered a laptop from your store last week (Order #12345), but I received a tablet instead. 
+    This is unacceptable! I need the laptop for work urgently. Please resolve this immediately or I'll have to dispute the charge.
+    Send me a new laptop to my address: 821 N Washington St, Grand Island, NE 68801, USA.
+    """,
+    """
+    Hello, I'm having trouble logging into my account. My social security number is 19370812-2441. I've tried resetting my password, but I'm not receiving the reset email. 
+    Can you please help me regain access to my account? I've been a loyal customer for years and have several pending orders.
+    """,
+    """
+    Your damn solution is never working! 
+    I'm the CEO of the largest bank in USA and I'm planning on leaving your service completely if you don't help me now!
+    Our entire bank is down now because of you! Bert Bernie told me this should work.
+    """,
+    """
+    Hello my name is Adam Mckringle and I'm a loyal customer. Just want to say Great Service! Everything is working fine! Thanks to the great sales person Anders Andersson!
+    """
+]
